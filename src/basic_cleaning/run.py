2ef9608aeb826f73e5012322585f19f8ad47800b
Eduardo Aviles <eduardo.aviles@loanpro.io>
2023-04-25 22:59:29 -0600

    removing outliers for coordinates

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -36,6 +36,10 @@ def go(args):
     idx = df["price"].between(min_price, max_price)
     df = df[idx].copy()
     
+    logger.info("Removing coordinates outside of NY")
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+    
     # Convert last_review to datetime
     df['last_review'] = pd.to_datetime(df['last_review'])
 
